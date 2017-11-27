07cb69d3b6314cb2e74820e18f9749a176419f03
zscgrhg <zscgrhg@qq.com>
2017-11-27 22:34:10 +0800

    oops

--- service-proxy/src/main/java/com/example/servicetransfer/web/HttpRouter.java
+++ service-proxy/src/main/java/com/example/servicetransfer/web/HttpRouter.java
@@ -26,7 +26,8 @@ import java.util.Map;
 public class HttpRouter {
 
     private static final int BUFFER_SIZE = 4096;
-    private static final String HTTP_POST = "post";
+    private static final String HTTP_POST = "POST";
+
     static {
         try {
             initSSL();
@@ -44,13 +45,13 @@ public class HttpRouter {
 
     public void forward(Locator locator, HttpServletRequest req, HttpServletResponse resp)
             throws UnavailableException, IOException {
-        boolean withBody = HTTP_POST.equalsIgnoreCase(req.getMethod());
+        boolean withBody = HTTP_POST.equals(req.getMethod().toUpperCase());
         forward(req, resp, locator, withBody);
     }
 
     public void forward(URL resourceURL, HttpServletRequest req, HttpServletResponse resp)
             throws UnavailableException, IOException {
-        boolean withBody = HTTP_POST.equalsIgnoreCase(req.getMethod());
+        boolean withBody = HTTP_POST.equals(req.getMethod().toUpperCase());
         forwardHttp(req, resp, resourceURL, withBody);
     }
 
